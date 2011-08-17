08b39fcbbb7e11fb15ae9f3e65ea0a7ff55d086c
Walter Vargas <walter@covetel.com.ve>
2011-08-17 12:03:59 -0430

    Soporte para configuracion vía variable de Entorno

--- CovetelLDAP/Covetel/LDAP.pm
+++ CovetelLDAP/Covetel/LDAP.pm
@@ -35,10 +35,13 @@ has config => (is => "ro", isa => "HashRef", lazy => 1, builder => "_build_confi
 
 sub _build_config {
 	my $self = shift; 
-	my $default_file = 'config.yml';
+	my $default_file = './config.yml';
 	if (!$self->config_file){
 		$self->config_file($default_file);
 	} 
+	if ($ENV{COVETEL_LDAP_CONFIG}){
+		$self->config_file($ENV{COVETEL_LDAP_CONFIG});
+	}
 	my $c = Config::Any::YAML->load($self->config_file);
 	return $c;	
 }
