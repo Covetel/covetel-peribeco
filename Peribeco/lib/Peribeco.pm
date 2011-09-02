12d28c10f78dd7620a63a52564a20569a6c25c8e
Carlos Paredes <cparedes@covetel.com.ve>
2011-09-02 00:18:47 -0430

    cambio de nombre del proyecto y validaciones

---
+++ Peribeco/lib/Peribeco.pm
@@ -0,0 +1,87 @@
+package Peribeco;
+use Moose;
+use namespace::autoclean;
+
+use Catalyst::Runtime 5.80;
+
+# Set flags and add plugins for the application
+#
+#         -Debug: activates the debug mode for very useful log messages
+#   ConfigLoader: will load the configuration from a Config::General file in the
+#                 application's home directory
+# Static::Simple: will serve static files from the application's root
+#                 directory
+
+use Catalyst qw/
+    Unicode::Encoding
+    -Debug
+    ConfigLoader
+    Static::Simple
+
+    Authentication
+    Session
+    Session::Store::FastMmap
+    Session::State::Cookie
+
+    Authorization::Roles
+    Authorization::ACL
+    
+    
+/;
+
+extends 'Catalyst';
+
+our $VERSION = '0.01';
+$VERSION = eval $VERSION;
+
+# Configure the application.
+#
+# Note that settings in diut.conf (or other external
+# configuration file that you set up manually) take precedence
+# over this when using ConfigLoader. Thus configuration
+# details given here can function as a default configuration,
+# with an external configuration file acting as an override for
+# local deployment.
+
+__PACKAGE__->config(
+    name => 'Peribeco',
+    # Disable deprecated behavior needed by old applications
+    disable_component_resolution_regex_fallback => 1,
+);
+
+__PACKAGE__->config(
+    'Plugin::ConfigLoader' => { file => 'configuracion.yml' },
+);
+
+# Start the application
+__PACKAGE__->setup();
+
+
+=head1 NAME
+
+Peribeco - Catalyst based application
+
+=head1 SYNOPSIS
+
+    script/diut_server.pl
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 SEE ALSO
+
+L<Peribeco::Controller::Root>, L<Catalyst>
+
+=head1 AUTHOR
+
+elsanto,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ Peribeco/root/static/js/application.js
@@ -0,0 +1,184 @@
+var tabla;
+var tabla2;
+var tabla3;
+var tabla4;
+var sel = true;
+
+
+$(document).ready(function(){
+    
+    $.ajaxSetup({ scriptCharset: "utf-8" , contentType: "application/json; charset=utf-8"});
+    
+   tabla = $("#lista_grupos").dataTable({
+		"sAjaxSource": '/ajax/grupos',
+		"bJQueryUI": true,
+ 		"oLanguage": {
+            "sUrl": "/static/js/dataTables.spanish.txt"
+        },
+	    "fnDrawCallback": function () {
+            my_hover();
+        }
+    });
+
+    tabla2 = $("#lista_personas").dataTable({
+		"sAjaxSource": '/ajax/personas',
+		"bJQueryUI": true,
+ 		"oLanguage": {
+            "sUrl": "/static/js/dataTables.spanish.txt"
+        },
+	    "fnDrawCallback": function () {
+            my_hover();
+        }
+    });
+
+    tabla3 = $("#lista_detalle_persona").dataTable({
+		"sAjaxSource": '/ajax/miembros_grupo',
+ 		"oLanguage": {
+            "sUrl": "/static/js/dataTables.spanish.txt"
+        },
+		"bJQueryUI": true,
+	    "fnDrawCallback": function () {
+            my_hover();
+        }
+    });
+
+    var gidNumber = $("span.gidNumber").html();
+    
+    tabla4 = $("#lista_miembros_grupo").dataTable({
+		"sAjaxSource": '/ajax/groupmembers/'+gidNumber,
+ 		"oLanguage": {
+            "sUrl": "/static/js/dataTables.spanish.txt"
+        },
+		"bJQueryUI": true,
+	    "fnDrawCallback": function () {
+            my_hover();
+        }
+    });
+
+
+    // Add member to group
+    $("form#form_add_member").submit(function(){ return false; });
+
+
+    // Boton agregar personas a un grupo. 
+    $("button#add_member").click(function(){
+        var uid = $("textarea#personas").val();
+        var personas = uid.split(',');
+        var gid = $("span.gidNumber").html();
+        var datos = ({'personas': personas, 'gid':gid});
+        var jsoon = $.JSON.encode(datos);
+			$.ajax({
+				url: "/ajax/grupos/add", 
+				type: "PUT",
+                data: jsoon, 
+				dataType: "json",
+				contentType: 'application/json',
+			    processData: false,
+				complete: function (data) {
+                    $("div#mensaje").html("Las personas fueron agregadas al grupo exitosamente");
+                    $("#personas").val('');
+                    $( "#mensaje" ).dialog({ buttons: { "Ok": function() { $(this).dialog("close"); } } });
+                    tabla4.fnReloadAjax();
+                }
+	        }); // Fin de ajax
+    });
+
+    $("button#remove_from_group").click(function(){
+        var uids = $("input:checked").getCheckboxValues();
+        var gid = $("span.gidNumber").html();
+        var datos = ({'personas': uids, 'gid':gid});
+        var jsoon = $.JSON.encode(datos);
+			$.ajax({
+				url: "/ajax/grupos/del", 
+				type: "DELETE",
+                data: jsoon, 
+				dataType: "json",
+				contentType: 'application/json',
+			    processData: false,
+				complete: function (data) {
+                    $("div#mensaje").html("Las personas fueron removidas del grupo exitosamente");
+                    $( "#mensaje" ).dialog({ buttons: { "Ok": function() { $(this).dialog("close"); } } });
+                    tabla4.fnReloadAjax();
+                }
+	        }); // Fin de ajax
+
+    });
+
+    $("button#select_all").click(function(){
+        if (sel){
+            $("input:checkbox").attr('checked', true);
+            $("button#select_all").html("Desmarcar todos");
+            sel = false;
+        } else {
+            $("input:checkbox").attr('checked', false);
+            $("button#select_all").html("Marcar todos");
+            sel = true;
+        
+        }
+    });
+
+    $("button#select_all_off").click(function(){
+        $("input:checkbox").attr('checked', false);
+    });
+
+    $("a.eliminar").click(function(){
+        return false;
+        $("div#all").append('<div id="mensaje"> </div>');
+        $("div#mensaje").html("Las personas fueron removidas del grupo exitosamente");
+        $( "#mensaje" ).dialog({ buttons: { "Ok": function() { $(this).dialog("close"); return false; } } });
+    
+    });
+
+    // Eliminar grupos
+    $("button#remove_group").click(function(){
+        var gids = $("input:checked").getCheckboxValues();
+        var datos = ({'gids': gids});
+        var jsoon = $.JSON.encode(datos);
+			$.ajax({
+				url: "/ajax/delete/groups", 
+				type: "DELETE",
+                data: jsoon, 
+				dataType: "json",
+				contentType: 'application/json; charset=utf-8',
+			    processData: false,
+				complete: function (data) {
+                    if (data.status == 200)
+                    {
+                        $("div#all").append('<div id="mensaje"> </div>');
+                        $("div#mensaje").html("Registros eliminados exitosamente");
+                        tabla.fnReloadAjax();
+                        $( "#mensaje" ).dialog({ buttons: { "Ok": function() { $(this).dialog("close"); } } });
+                    }
+                }
+	        }); // Fin de ajax
+
+    });
+
+
+    $("button#remove_person").click(function(){
+        var uids = $("input:checked").getCheckboxValues();
+        var datos = ({'uids': uids});
+        var jsoon = $.JSON.encode(datos);
+			$.ajax({
+				url: "/ajax/delete/persons", 
+				type: "DELETE",
+                data: jsoon, 
+				dataType: "json",
+				contentType: 'application/json; charset=utf-8',
+			    processData: false,
+				complete: function (data) {
+                    if (data.status == 200)
+                    {
+                        $("div#all").append('<div id="mensaje"> </div>');
+                        $("div#mensaje").html("Registros eliminados exitosamente");
+                        tabla2.fnReloadAjax();
+                        $( "#mensaje" ).dialog({ buttons: { "Ok": function() { $(this).dialog("close"); } } });
+                    }
+                }
+	        }); // Fin de ajax
+
+    });
+    
+    $("#uid_field").parent().hide();
+
+});
