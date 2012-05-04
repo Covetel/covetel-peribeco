232443b985aab753c1eae9d44866993d308efa6d
Walter Vargas <walter@covetel.com.ve>
2012-05-03 23:28:29 -0430

    scripts con curl para pruebas

---
+++ curl/curl-ajax-personas.sh
@@ -0,0 +1,2 @@
+#curl -H 'Content-Type: text/x-yaml' -X GET --cookie cjar http://localhost:3000/ajax/personas
+curl -H 'Content-Type: text/xml' -X GET --cookie cjar http://localhost:3000/ajax/personas
