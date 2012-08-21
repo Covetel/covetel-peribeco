b3e2b59cc32d88e3ed8a9ef6042209967d6c24d7
Carlos Paredes <cparedes@covetel.com.ve>
2012-08-21 12:08:41 -0430

    Agregada validación para modulo de Grupos activo
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- Peribeco/lib/Peribeco/Controller/Grupos.pm
+++ Peribeco/lib/Peribeco/Controller/Grupos.pm
@@ -32,90 +32,109 @@ sub index :Path :Args(0) {
 
 sub crear : Local  : FormConfig {
     my ( $self, $c ) = @_;
-    if ($c->assert_user_roles(qw/Administradores/)) { 
+    if ($c->config->{'Modulos'}->{'Grupos'} == 1 ) {
+        if ($c->assert_user_roles(qw/Administradores/)) { 
+        $c->stash->{modules} = $c->config->{'Modulos'}; 
+
         # Clases para los campos requeridos. 
-    my $form = $c->stash->{form};
+        my $form = $c->stash->{form};
         $form->auto_constraint_class( 'constraint_%t' );
+        
+        if ( $form->submitted_and_valid ) {
+                my $grupo       = $c->req->param("nombre");
+                my $descripcion = $c->req->param("descripcion");
     
-    if ( $form->submitted_and_valid ) {
-            my $grupo       = $c->req->param("nombre");
-            my $descripcion = $c->req->param("descripcion");
-
-        #Envia el nuevo Usuario a LDAP
-        my $ldap = Covetel::LDAP->new;
-        my $group = Covetel::LDAP::Group->new({ 
-            nombre => $grupo, 
-            description => $descripcion, 
-                    ldap => $ldap 
-            });
-
-        $c->log->debug($group->dn());
-
-        if ($group->add()){
-            $c->stash->{mensaje} = "El grupo $grupo ha sido
-            agregado exitosamente";
-            $c->stash->{sucess} = 1;
-            }else{
+            #Envia el nuevo Usuario a LDAP
+            my $ldap = Covetel::LDAP->new;
+            my $group = Covetel::LDAP::Group->new({ 
+                nombre => $grupo, 
+                description => $descripcion, 
+                        ldap => $ldap 
+                });
+    
+            $c->log->debug($group->dn());
+    
+            if ($group->add()){
+                $c->stash->{mensaje} = "El grupo $grupo ha sido
+                agregado exitosamente";
+                $c->stash->{sucess} = 1;
+                }else{
+                $c->stash->{error} = 1;
+                $c->stash->{mensaje} = "<strong> Error Crítico en LDAP:</strong>".$group->ldap->error_str();
+            }
+            } elsif ($form->has_errors && $form->submitted) {
+            my @err_fields = $form->has_errors;
+                    my $label = $form->get_field($err_fields[0])->label; 
+    
             $c->stash->{error} = 1;
-            $c->stash->{mensaje} = "<strong> Error Crítico en LDAP:</strong>".$group->ldap->error_str();
+            $c->stash->{mensaje} = "Ha ocurrido un error en el campo <span class='strong'> $label </span> ";
         }
-        } elsif ($form->has_errors && $form->submitted) {
-        my @err_fields = $form->has_errors;
-                my $label = $form->get_field($err_fields[0])->label; 
-
-        $c->stash->{error} = 1;
-        $c->stash->{mensaje} = "Ha ocurrido un error en el campo <span class='strong'> $label </span> ";
-    }
+        }
+    }else{
+        $c->res->body('Modulo no disponible');
     }
 }
 
 sub lista : Local {
     my ( $self, $c ) = @_;
-    if ($c->assert_user_roles(qw/Administradores/)) { 
-    my $ldap = Covetel::LDAP->new;
-    my @lista = $ldap->group();
-    $c->stash->{grupos} = \@lista;
+    if ($c->config->{'Modulos'}->{'Grupos'} == 1 ) {
+        if ($c->assert_user_roles(qw/Administradores/)) { 
+            my $ldap = Covetel::LDAP->new;
+            my @lista = $ldap->group();
+            $c->stash->{grupos} = \@lista;
+            $c->stash->{modules} = $c->config->{'Modulos'}; 
+        }
+    }else{
+        $c->res->body('Modulo no disponible');
     }
 }
 
 sub eliminar : Local {
     my ( $self, $c, $gid) = @_;
-    if ($c->assert_user_roles(qw/Administradores/)) { 
-    my $ldap = Covetel::LDAP->new;
-    my $grupo = $ldap->group({gidNumber => $gid});
-    if ($grupo){
-        if($grupo->del()){
-            $c->stash->{mensaje} = "El grupo ".$grupo->nombre." ha sido eliminado exitosamente";
+    if ($c->config->{'Modulos'}->{'Grupos'} == 1 ) {
+        if ($c->assert_user_roles(qw/Administradores/)) { 
+            my $ldap = Covetel::LDAP->new;
+            my $grupo = $ldap->group({gidNumber => $gid});
+            if ($grupo){
+                if($grupo->del()){
+                    $c->stash->{mensaje} = "El grupo ".$grupo->nombre." ha sido eliminado exitosamente";
+                }
+            } else {
+                $c->stash->{error} = 1;
+                $c->stash->{mensaje} = "No se encontro el grupo";
+            }
         }
-    } else {
-            $c->stash->{error} = 1;
-            $c->stash->{mensaje} = "No se encontro el grupo";
-    }
+    }else{
+        $c->res->body('Modulo no disponible');
     }
 }
 
 sub detalle : Local {
     my ( $self, $c, $gid) = @_;
-    if ($c->assert_user_roles(qw/Administradores/)) { 
-    my $ldap = Covetel::LDAP->new;
-    my $grupo = $ldap->group({gidNumber => $gid});
+    if ($c->config->{'Modulos'}->{'Grupos'} == 1 ) {
+        if ($c->assert_user_roles(qw/Administradores/)) { 
+            my $ldap = Covetel::LDAP->new;
+            my $grupo = $ldap->group({gidNumber => $gid});
     
-    if($grupo) {
-        my $nombre = $grupo->nombre();
-        my $desc = $grupo->description();
+            if($grupo) {
+                my $nombre = $grupo->nombre();
+                my $desc = $grupo->description();
 
-        utf8::decode($nombre);
-        utf8::decode($desc); 
+                utf8::decode($nombre);
+                utf8::decode($desc); 
 
-        $grupo->nombre($nombre);
-        $grupo->description($desc);
+                $grupo->nombre($nombre);
+                $grupo->description($desc);
 
-        $c->stash->{grupo} = $grupo;
+                $c->stash->{grupo} = $grupo;
+            }else{
+                $c->res->body('No se encuentra el Grupo')
+            }
+        }
     }else{
-        $c->res->body('No se encuentra el Grupo')
+        $c->res->body('Modulo no disponible');
     }
-    }
-   }
+}
 
 
 =head1 AUTHOR
