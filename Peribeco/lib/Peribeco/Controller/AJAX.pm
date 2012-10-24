7070ca3cf245e2ef8ea40513d1a80a7e31ddb2f5
Carlos Paredes <cparedes@covetel.com.ve>
2012-10-24 17:16:30 -0430

    Modificando forma en que se muestran los miembros de una lista en la tabla
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- Peribeco/lib/Peribeco/Controller/AJAX.pm
+++ Peribeco/lib/Peribeco/Controller/AJAX.pm
@@ -205,7 +205,7 @@ sub listas_GET {
                     '<input type="checkbox" name="del" value="'.$_->get_value($id).'">', 
                     $_->get_value($mail), 
                     &utf8_decode($_->get_value($desc)), 
-                    '<div class="members_div">' . $self->remove_domain($_->get_value($member_mail)) . '</div>',
+                    '<div class="members_div" id="'.$_->get_value($id).'">' . $self->remove_domain($_->get_value($member_mail)) . '</div>',
                     '<a href="/correo/listas/detalle/' . $_->get_value($id) . '"> Ver detalle </a>',
                 ]
                 } $mesg->entries,
